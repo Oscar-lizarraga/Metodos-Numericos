f6de11e21908f900ba1caafd4c727b7deda47b8b
Oscar-lizarraga <421088607@pcpuma.acatlan.unam.mx>
2021-10-16 20:46:49 -0500

    Horner modificacion

--- determinante_nxn.c
+++ determinante_nxn.c
@@ -1,40 +1,128 @@
-//Programa para calcular el determinante de una matriz de nxn
-
+/***********************************************************
+ * Metodos numericos, Programa para el calculo del determinante 
+ * por recursividad de una matriz de nxn
+ * 
+ * Fecha de ultima revision: 16/10/21
+ * 
+ * Autor:
+ * Lizarraga Pelayo Oscar
+ * *********************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+double** ConstruirMatriz(int, int);
+double CalcularDeterminante(int, int, double**);
+void LeerMatriz(char, int, int, double**);
+void ImprimirMatriz(int, int, double**);
+void DestruirMatriz(int, double**);
 void LeerEntero(int* a);
-void LeerArreglo(int **m, int c, int r);
-int ConstruirArreglo(int **m, int c, int r);
-void LimpiarCadena(char *cadena);
 
-
-int main(int argc, char const *argv[])
+int main(void)
 {
-	int** m;
-	int c = 0, r = 0;
+	double **A;
+	int M, N;
+	printf("Renglones de la matriz: \n");
+	LeerEntero(&M);
+	printf("Columnas de la matriz: \n");
+	LeerEntero(&N);
 
-	printf("\n\t Ingrese el numero de columnas: ");
-	LeerEntero(&c);
-	printf("\n\t Ingrese el numero de renglones: ");
-	LeerEntero(&r);
+	printf("Construyendo la matriz A... \n");
+	A = ConstruirMatriz(M, N);
 
-	if(ConstruirArreglo(m,c,r)){
-		printf("\t Se creo el arreglo\n\t");
+	printf("Introduzca los elementos ...\n");
+	LeerMatriz('A', M, N, A);
 
+	printf("Los elementos de la matriz son: \n");
+	ImprimirMatriz(M, N, A);
 
-	}
-	else{
-		printf("ERROR: No hay memoria suficiente! ");
-	}
-	LeerArreglo(m,c,r);
+	printf("\nEl determinante es: %.5lf"	,CalcularDeterminante(M,N,A));
+	DestruirMatriz(M, A);
 
-
-
+	return 0;
 }
 
+double CalcularDeterminante(int m, int n, double** mat)
+{
+	if(n == 0 || n != m) return 0.0;			//Si la matriz no es cuadrada su det es 0
+	if(n == 1 && m == 1) return mat[0][0];		//Si la matriz es de dim 1, su determinante es el mismo elemento
+	double determinante = 0.0;
+	for (int k = 0; k < n; k++)
+	{
+		double** mat_a = ConstruirMatriz(m-1,n-1); //crearemos una matriz con una dimension menor
+		for (int i = 0; i < m-1 ; i++)
+		{
+			for (int j = 0; j < n-1; j++)	//Aqui asignaremos los valores de la matriz original a la matriz con dimension menor
+			{
+				if(j < k){mat_a[i][j] = mat[i+1][j];}		
+				else{mat_a[i][j] = mat[i+1][j+1];}		
+			}
+		}
+		if (k % 2 == 0){ // aqui tomaremos el cambio de signo para el coeficiente 
+			determinante+= mat[0][k]*CalcularDeterminante(m-1,n-1,mat_a); //aqui hacemos la llamada recursiva
+		}
+		else{
+			determinante-= mat[0][k]*CalcularDeterminante(m-1,n-1,mat_a);
+		}
+	}
+	return determinante;
+}
+
+
+/*Este metodo crea una matriz dinamica, primero se crea una matriz aux que sera devuelta
+si no se pudo reservar la memoria con longitud de m-renglones entonces su valor sera NULL y
+se devolvera, despues en cada renglon reservamos memoria para cada columna*/
+double** ConstruirMatriz(int m, int n)
+{
+	double** mat = (double**)calloc(m, sizeof(double*));	
+	if (mat == NULL) exit(0);
+	for (int i = 0; i < m; i++)
+	{
+		mat[i] = (double*)calloc(n, sizeof(double));
+		if (mat[i] == NULL)
+		{
+			printf("Error de asignacion de memoria\n");
+			exit(0);
+		}
+	}
+	return mat;
+}
+
+void LeerMatriz(char ch, int m, int n, double **mat)
+{
+	char str[128];
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%c(%d,%d) = ?", ch, i + 1, j + 1);
+			fgets(str, sizeof(str), stdin);
+			mat[i][j] = (double)atof(str);
+		}
+	}
+}
+
+void DestruirMatriz(int m, double** mat)
+{
+	for (int i = 0; i < m; i++)
+	{
+		free(mat[i]);
+	}
+	free(mat);
+}
+
+
+void ImprimirMatriz(int m, int n, double **mat)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("\t%.2lf", mat[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 void LeerEntero(int *a)
 {
@@ -46,43 +134,6 @@ void LeerEntero(int *a)
 	}
 }
 
-void LeerArreglo(int** m, int c, int r)
-{
-	char* matriz = (char*)calloc(2*c,sizeof(char));			
-	for (int i = 0; i < c; i++)
-	{
-		fgets(matriz,(2*c)+1,stdin);
-		fflush(stdin);
-
-		for (int j = 0; j < c; j++)
-		{
-			printf("%d\n",matriz[j*2] - 48);
-			m[i][j] = (matriz[j*2] -48);
-		}	
-	}
-}
-
-int ConstruirArreglo(int** m, int c, int r)
-{
-	m = (int**)calloc(c, sizeof(int*));
-	if(m == NULL) return 0;
-	for (int i = 0; i < c; i++)
-	{
-		m[i] = (int*)calloc(r,sizeof(int));
-		if(m[i] == NULL){
-			return 0;
-		}
-	}
-}
-
-void LimpiarCadena(char *cadena)
-{
-	int longitud = strlen(cadena);
-	if (longitud > 0 && (cadena[longitud - 1] == '\n'))
-	{
-		cadena[longitud - 1] = '\0';
-	}
-}
 
 
 
--- horner.c
+++ horner.c
@@ -2,25 +2,26 @@
 
 int main(int argc, char const *argv[])
 {
-	int n;
-	float x;
+	int n; 				//grado del polinomio
+	float x;			//valor de x en el polinomio
+	
 	printf("\n\tIngrese el grado del polinomio: ");
-	scanf("%i",&n);
+	scanf("%d",&n);     
 
-	n++;
-	float a[n];
+	n++;				//Le sumamos 1, porque 
+	float a[n];			//de 0 a n elementos
 	for (int i = 0; i < n; i++)
 	{
 		printf("\n\tcoeficiente x^%i: ",i);
 		scanf("%f",&a[i]);
 	}
 	printf("\n\tIngrese el valor para x: ");
-	scanf("%i",&x);
-	float p = a[n];
+	scanf("%f",&x);
+	float p = a[n-1]; //le asignamos el valor del ultimo coeficiente porque A(n) es la ultima iteracion
 
 	for ( int i = (n-1); i > 0; i--)
 	{
-		p = a[i] + x*p;
+		p = a[i] + x*p; //esto calculara la P(n) con la P(n-1)   coef + (x)P(n-1);
 	}
 	printf("\n\t%.2f",p);
 	return 0;
